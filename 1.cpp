62733fdc3010dbf61257b4f68300f6acce81c4f7
yoonkyung <ykha9911@gmail.com>
2020-08-08 22:53:12 +0900

    answer

--- 1.cpp
+++ 1.cpp
@@ -5,21 +5,24 @@ using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
     int answer = 0;
-    int j=0;
+    int index=0;
     vector<int> v;
 
     for(int i=0; i<moves.size(); i++) {
+        index = moves[i]-1;
         for(int j=0; j<board.size(); j++) {
-            if(board[j][moves[i]-1]!=0) {
-                v.push_back(board[j][moves[i]-1]);
-                board[j][moves[i]-1] = 0;
+            if(board[j][index]!=0) {
+                v.push_back(board[j][index]);
+                board[j][index] = 0;
+                break;
             }
-        }
-        if(i!=0) {
-            if(v[i] == v[i-1]) {
-                answer += 2;
-                v.pop_back();
-                v.pop_back();
+        } if(v.size()>=2) {
+            for(int y=0; y<v.size(); y++) {
+                if(v[y] == v[y-1]) {
+                    answer += 2;
+                    v.pop_back();
+                    v.pop_back();
+                }
             }
         }
     }
